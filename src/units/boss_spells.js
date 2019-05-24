ab246caef1d3ea5b0356cb25c57a0b4023dec861
Andrew <andrew.hamacher@gmail.com>
2019-05-24 08:50:07 -0700

    Add Flare

--- src/units/boss_spells.js
+++ src/units/boss_spells.js
@@ -5,9 +5,22 @@ class Spells {
     this.game = options.game;
   }
 
-  flare(){
+  flare() {
     const { party } = this.game;
-    
+    const randomDmg = (Math.random() * 5) + 10;
+    setTimeout(() => {
+
+      party.forEach(member => {
+        if (member.currentHp > 0 && this.currentHp > 0) {
+          member.currentHp = member.currentHp - randomDmg;
+        }
+        if (member.currentHp < 0) {
+          member.currentHp = 0;
+        }
+      });
+    }, 2500);
   }
 }
 
+
+module.exports = Spells;
