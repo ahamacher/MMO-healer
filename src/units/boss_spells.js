b13d66afce15b96e185976df11f822033f3b6f1f
Andrew <andrew.hamacher@gmail.com>
2019-05-24 08:40:57 -0700

    Initialized Boss_spells

---
+++ src/units/boss_spells.js
@@ -0,0 +1,13 @@
+class Spells {
+  constructor(options){
+    this.target = options.target;
+    this.boss = options.boss;
+    this.game = options.game;
+  }
+
+  flare(){
+    const { party } = this.game;
+    
+  }
+}
+
